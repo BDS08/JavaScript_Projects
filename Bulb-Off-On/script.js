9886ff8b71848831da599048542e49a1b2bf5d0c
Badal kumar singh <jpbadalsingh8899@gmail.com>
2022-03-02 01:01:52 +0530

    Add files via upload

---
+++ Bulb-Off-On/script.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById("btn") 
+let bulb = document.getElementById("img") 
+btn.addEventListener("click",togglebulb);;
+
+function togglebulb(e){
+    
+    if(btn.textContent.includes('On')){
+        bulb.src = 'lightOn.jpg'
+        btn.textContent = 'Turn Off'
+    }
+    else{
+        bulb.src = 'lightOff.jpg'
+        btn.textContent = 'Turn On'
+    }
+    
+
+}
